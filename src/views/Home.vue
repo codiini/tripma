55863219fc6547b93f7cb68bdb6d5b527f17df12
Iniubong <iniubongobonguko2018@gmail.com>
2021-03-28 15:26:21 +0100

    integrated all compnents for the landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hero />
+    <v-container>
+      <FlightDeals />
+      <UniquePlaces />
+      <Testimonials />
+    </v-container>
+    <Footer />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Hero from "@/components/landing-page/Hero";
+import FlightDeals from "@/components/landing-page/FlightDeals";
+import UniquePlaces from "@/components/landing-page/UniquePlaces";
+import Testimonials from "@/components/landing-page/Testimonials";
+import Footer from "../components/landing-page/Footer.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Hero,
+    FlightDeals,
+    UniquePlaces,
+    Testimonials,
+    Footer,
+  },
+};
 </script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;1,300&display=swap");
+* {
+  font-family: "Nunito Sans", sans-serif;
+  /* outline: 1px solid red; */
+}
+</style>
