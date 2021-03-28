52e44462ee2fd9c5e6e85f3042f0b3f2ab5a41ee
Iniubong <iniubongobonguko2018@gmail.com>
2021-03-28 15:49:56 +0100

    upcoming trip component

---
+++ src/components/UpcomingTrips.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card>
+    <v-row align="center" justify="center" no-gutters>
+      <v-col cols="12" sm="3">
+        <v-img
+          height="100"
+          width="100"
+          class="rounded-md"
+          src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+        >
+        </v-img>
+      </v-col>
+      <v-col class="flex items-center">
+        <div>
+          <p class="text-base"><slot name="trip-name"></slot></p>
+          <small class="text-xs text-gray-500"
+            ><slot name="trip-duration"></slot
+          ></small>
+        </div>
+        <!-- <div class="flex justify-end pr-3">
+          <v-btn small color="primary" class="rounded-lg" elevation="0"
+            >Details</v-btn
+          >
+        </div> -->
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+</style>
