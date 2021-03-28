b42013cf2b3e447b93edfd6f99710e844562b1e5
Iniubong <iniubongobonguko2018@gmail.com>
2021-03-28 23:54:57 +0100

    modify compoonent to look better

--- src/components/UpcomingTrips.vue
+++ src/components/UpcomingTrips.vue
@@ -3,15 +3,13 @@
     <v-row align="center" justify="center" no-gutters>
       <v-col cols="12" sm="3">
         <v-img
-          height="100"
-          width="100"
           class="rounded-md"
           src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
         >
         </v-img>
       </v-col>
       <v-col class="flex items-center">
-        <div>
+        <div class="p-2">
           <p class="text-base"><slot name="trip-name"></slot></p>
           <small class="text-xs text-gray-500"
             ><slot name="trip-duration"></slot
